344b83753fed628fd2b9ff9b5133e0bb9e48f004
gdarves <gil.darves@unifyconsulting.com>
2020-05-04 19:06:34 -0700

    snapshot final changes

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -70,6 +70,32 @@ def list_volumes(project):
 def instances():
     """Commands for instances"""
 
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag project:<name>)")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created By Snapshot 3000")
+
+            print("Starting {0}".format(i.id))
+
+            i.start()
+            i.wait_until_running()
+
+    print("Job's done!")
+    return
+
 
 @instances.command('list')
 @click.option('--project', default=None,
